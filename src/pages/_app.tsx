6bd011cf4c9237d076874d60bc564c000f5e06d2
David Zigmund <david.zigmund@beafuture.com>
2023-06-27 17:44:31 +0200

    localization added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { csCZ as materialCsCz} from '@mui/material/locale';
+import {  csCZ as dataGridCsCz} from '@mui/x-data-grid';
 
+
+const theme = createTheme(
+  {
+    palette: {
+      primary: { main: '#1976d2' },
+    },
+  },
+  materialCsCz,
+  dataGridCsCz
+);
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>;
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { Button, Table } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { api } from "~/utils/api";
 
