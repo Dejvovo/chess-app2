daf47422d969d2e24c364cd1c6ebb3440be2d504
David Goliard <david.goliard@beafuture.com>
2023-10-13 16:19:24 +0200

    Text (bez diakritiky added)

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -4,7 +4,7 @@ import {SearchOutlined} from "@ant-design/icons";
 export const FilterForm = ({onFormFinish}: {onFormFinish: (data: {name: string}) => void}) => <>
     <div style={{padding: '0px 0px 10px 0px'}}>
         <Form onFinish={onFormFinish} layout={'inline'}>
-            <Form.Item label={'Jméno hráče'} name={'name'}>
+            <Form.Item label={'Jméno hráče (bez diakritiky):'} name={'name'}>
                 <Input />
             </Form.Item>
             <Form.Item>
--- src/components/FilterFormMobile.tsx
+++ src/components/FilterFormMobile.tsx
@@ -4,7 +4,7 @@ import {SearchOutlined} from "@ant-design/icons";
 export const FilterFormMobile = ({onFormFinish}: {onFormFinish: (data: {name: string}) => void}) => <>
     <div>
         <Form onFinish={onFormFinish} style={{display: 'flex', justifyContent: 'center', padding: '4px 4px 12px 4px', marginBottom:'12px', borderBottom: '1px solid gray'}}>
-            <label style={{marginTop: '4px', height: "min-content"}}>Jméno hráče:</label>
+            <label style={{marginTop: '4px', height: "min-content"}}>Jméno hráče (bez diakritiky):</label>
             <div style={{display: 'flex', gap: '10px', height: "32px"}}>
                 <Form.Item name={'name'} style={{width: '200px'}}><Input name={'name'} style={{width: '200px', height: 'min-content', margin: '0 8px'}}/></Form.Item>
                 <Button type="primary" icon={<SearchOutlined />} htmlType="submit"></Button>
